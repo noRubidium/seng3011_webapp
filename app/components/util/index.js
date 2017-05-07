bbb3f7b7737f2b9069a5f24bd0c7c06b80d109eb
vaishnavi-bapat <vaishnavi.bapat@gmail.com>
2017-05-07 15:23:03 +1000

    extracted data from abs json blob

--- app/components/util/index.js
+++ app/components/util/index.js
@@ -7,30 +7,59 @@ var data = require('./dummydata.json');
 
 const ReactHighcharts = require('react-highcharts'); // Expects that Highcharts was loaded in the code.
 
-export default class StockChart extends React.Component{
+class StockChart extends React.Component{
 
-    // getCategories(data) {
-    //
-    //   return array
-    // }
+    getCategoriesData(retailData) {
+      var categoryArray = [];
+      for (var key in retailData) {
+          categoryArray.push(this.getDataArray(retailData[key].regional_data[0].data));
+      }
+      return categoryArray;
+    }
+
+    getCategoryNames(retailData) {
+      var categories = [];
+      for (var key in retailData) {
+          categories.push(retailData[key].category);
+      }
+      return categories;
+    }
+
+    getDataArray(categoryArray) {
+      var dataPointArray = [];
+      for (var dataPoint in categoryArray) {
+        dataPointArray.push(categoryArray[dataPoint].turnover);
+      }
+      return dataPointArray;
+    }
 
     getData() {
       return data
     }
+
+    createConfig(dataArray, seriesNames) {
+
+    }
+
     //Create the div which the chart will be rendered to.
     render () {
-      console.log(this.getData().MonthlyRetailData);
+      var retailData = this.getData().MonthlyRetailData;
+      var dataArray = this.getCategoriesData(retailData);
+      var seriesNames = this.getCategoryNames(retailData);
+
+      this.crateConfig(dataArray, seriesNames);
+
       const config = {
         xAxis: {
           categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         },
         series: [{
           name:"Pranav",
-          data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
+          data: dataArray[0]
         },
         {
           name:"Vaishnavi",
-          data: [129.9, 171.5, 1106.4, 1219.2, 1144.0, 11761.0, 11315.6, 1418.5, 2116.4, 1914.1, 2195.6, 4514.4]
+          data: dataArray[1]
         }]
 
       };
