67be142f9d3d159c35ee220d6f0781b4c5981af4
norubidium <mshen@cse.unsw.edu.au>
2017-05-07 20:30:07 +1000

    ha

--- app/actions/company/index.js
+++ app/actions/company/index.js
@@ -1,8 +1,6 @@
 import X2JS from 'x2js';
 
 // refer to the counter action
-const COMPANY_PRICE_LOADING = 'COMPANY_PRICE_LOADING';
-const COMPANY_PRICE_LOADED = 'COMPANY_PRICE_LOADED';
 const COMPANY_DATA_LOADING = 'COMPANY_DATA_LOADING';
 const COMPANY_DATA_LOADED = 'COMPANY_DATA_LOADED';
 const COMPANY_NEWS_LOADING = 'COMPANY_NEWS_LOADING';
@@ -11,8 +9,6 @@ const COMPANY_STATS_LOADING = 'COMPANY_STATS_LOADING';
 const COMPANY_STATS_LOADED = 'COMPANY_STATS_LOADED';
 
 export const actionTypes = {
-  COMPANY_PRICE_LOADING,
-  COMPANY_PRICE_LOADED,
   COMPANY_DATA_LOADING,
   COMPANY_DATA_LOADED,
   COMPANY_NEWS_LOADING,
@@ -39,8 +35,8 @@ const async_action_xml = (loading_action, loaded_action_type, dispatch, url) => 
   const x2js = new X2JS();
   console.log(x2js);
   fetch(url)
-    .then(response => response.text())
-    .then(str => {
+    .then((response) => response.text())
+    .then((str) => {
       const data = (x2js.xml2js(str));
       dispatch({
         type: loaded_action_type,
@@ -48,18 +44,6 @@ const async_action_xml = (loading_action, loaded_action_type, dispatch, url) => 
       });
     });
 };
-
-export function load_company_price (company_code, dispatch) {
-  async_action(
-    {
-      type: COMPANY_PRICE_LOADING,
-      payload: { company_code },
-    },
-    COMPANY_PRICE_LOADED,
-    dispatch,
-    `http://data.asx.com.au/data/1/share/${company_code.slice(0,3)}/prices?interval=daily&count=1`
-  );
-}
 
 export function load_company_info (company_code, dispatch) {
   async_action(
@@ -69,11 +53,20 @@ export function load_company_info (company_code, dispatch) {
     },
     COMPANY_DATA_LOADED,
     dispatch,
-    `http://api.kaiworship.xyz/companies/${company_code}`
+    `http://api.kaiworship.xyz/company/${company_code}`
   );
 }
 
 export function load_company_stats (company_code, dispatch) {
+  const today = new Date();
+  const lower_date = new Date(2003, 1, 1);
+
+  const one_day = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+  const diff_date = Math.round(Math.abs((lower_date.getTime() - today.getTime())/(one_day)));
+
+  const dd = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate();
+  const mm = today.getMonth() + 1 < 10 ? `0${today.getMonth() + 1}` : today.getMonth() + 1; //January is 0!
+  const yyyy = today.getFullYear();
   async_action(
     {
       type: COMPANY_STATS_LOADING,
@@ -81,7 +74,7 @@ export function load_company_stats (company_code, dispatch) {
     },
     COMPANY_STATS_LOADED,
     dispatch,
-    `http://174.138.67.207/InstrumentID/${company_code}/DateOfInterest/2012-12-10/List_of_Var/CM_Return,AV_Return/Upper_window/5/Lower_window/3`
+    `http://174.138.67.207/InstrumentID/${company_code}/DateOfInterest/${yyyy}-${mm}-${dd}/List_of_Var/Trading_Info/Upper_window/0/Lower_window/${diff_date}`
   );
 }
 
