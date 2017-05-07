48fe5471a90ac1f9094a0eadb69aa7753a7f0768
Jasonvooo <jasonvo1997@gmail.com>
2017-05-07 19:10:17 +1000

    data is there

--- app/components/CompanyPrice/index.js
+++ app/components/CompanyPrice/index.js
@@ -14,16 +14,17 @@ export default class CompanyPrice extends LoadableComponent {
     super(props);
     const { code, dispatch } = this.props;
     load_company_price(code, dispatch);
+    console.log('heree',this);
   }
 
   render () {
     this.loaded_object = (<div class="container">
-      <table className="table table-bordered">
+      <table className="table table-striped">
         <tbody>
           <tr>
             <td rowspan="2">
               {this.props.close_price}
-              Change {this.props.change_price} ({this.props.change_percentage})
+              Change {this.props.change_price} ({this.props.change_in_percent})
               Close date {this.props.close_date}
             </td>
             <td rowspan="2">
