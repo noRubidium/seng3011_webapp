50e2fc19c022eb6bad42b6adc59bf6f024fc991b
norubidium <mshen@cse.unsw.edu.au>
2017-05-08 23:55:29 +1000

    css white background for now

--- app/containers/Company/index.js
+++ app/containers/Company/index.js
@@ -13,7 +13,9 @@ export default class Company extends React.Component {
   render () {
     const { company_id } = this.props.match.params;
     return (
-      <div>
+      <div style={{background:'white',
+      WebkitTransition: 'height 2s',
+      transition: 'height 2s'}}>
         <div>
           <CompanyInfo cid={company_id}/>
         </div>
@@ -21,7 +23,7 @@ export default class Company extends React.Component {
           <div className='col-6 col-sm-4 placeholder'> {/*need styling */}
             <CompanyNews cid={company_id}/>
           </div>
-          <div className='col-6 col-sm-8 placeholder'>
+          <div className='col-6 col-sm-8 placeholder' style={{minHeight: '500px'}}>
             <CompanyStats cid={company_id}/>
           </div>
         </section>
