982a68cb63fea9bb8e5ac1dcefc02d09d1f8e903
Jessica Theodosius <z5057844@student.unsw.edu.au>
2017-05-20 17:30:33 +1000

    indentation

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -9,19 +9,19 @@ export default class Home extends React.Component {
     return (
       <div className='front-page'>
         <div className='stockoverflow'> StockOverflow </div>
-				<div className='search-by-company'>
-					<div className='input-group'>
-			      <input type='text' className='form-control' placeholder='Search for a company'/>
-			      <span className='input-group-btn'>
-			        <button className='btn btn-default' type='button'>Go!</button>
-			      </span>
-					</div>
-				</div>
-				<div className='search-industries'>
-					Don''t know what to find?&nbsp;
-					<Link to='/industries'>Click to search on industries</Link>
-				</div>
-			</div>
+        <div className='search-by-company'>
+          <div className='input-group'>
+            <input type='text' className='form-control' placeholder='Search for a company'/>
+            <span className='input-group-btn'>
+              <button className='btn btn-default' type='button'>Go!</button>
+            </span>
+          </div>
+        </div>
+        <div className='search-industries'>
+          Don''t know what to find?&nbsp;
+          <Link to='/industries'>Click to search on industries</Link>
+        </div>
+      </div>
     );
   }
 }
