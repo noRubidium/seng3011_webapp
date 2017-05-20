300b6b22778dba68240e6be591c4541803dda11d
Jessica Theodosius <z5057844@student.unsw.edu.au>
2017-05-20 20:42:04 +1000

    popup bby

--- app/components/Compare/popup.js
+++ app/components/Compare/popup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Select from 'react-select';
 
 class PopupContent extends React.Component {
@@ -11,21 +12,23 @@ class PopupContent extends React.Component {
   }
 
   handleSelectChange(value) {
-		this.setState({ value });
+		this.props.parent.setState({ value });
 	}
 
   render () {
     const options = [
-      { value: 'MYR', label: 'MYR' },
-      { value: 'DMP', label: 'DMP' },
-      { value: 'WOW', label: 'WOW' },
-      { value: 'WES', label: 'WES' }
+      { value: 'MYR.AX', label: 'MYR' },
+      { value: 'DMP.AX', label: 'DMP' },
+      { value: 'WOW.AX', label: 'WOW' },
+      { value: 'WES.AX', label: 'WES' }
     ];
+
+    const { parent } = this.props;
 
     return (
       <Select multi
             simpleValue
-            value={this.state.value}
+            value={parent.state.value}
             placeholder="Select companies"
             options={options}
             onChange={this.handleSelectChange}
@@ -34,8 +37,22 @@ class PopupContent extends React.Component {
   }
 }
 
-export default class ComparePopup extends React.Component {  
+export default class ComparePopup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: []
+    };
+    this.compare = this.compare.bind(this);
+  }
+
+  compare() {
+    $('.modal-backdrop').hide();
+  }
+
   render () {
+    const compareLink = `/compare/${this.state.value}`;
+
     return (
     <div className='modal fade' id='compare-popup' tabIndex='-1' role='dialog'>
       <div className='modal-dialog' role='document'>
@@ -47,11 +64,13 @@ export default class ComparePopup extends React.Component {
             <h4 className='modal-title'>Compare</h4>
           </div>
           <div className='modal-body'>
-            <PopupContent/>
+            <PopupContent parent={this}/>
           </div>
           <div className='modal-footer'>
             <button type='button' className='btn btn-default' data-dismiss='modal'>Close</button>
-            <button type='button' className='btn btn-primary'>Compare</button>
+            <Link to={compareLink} onClick={this.compare}>
+              <button type='submit' className='btn btn-primary compare-popup-button'>Compare</button>
+            </Link>
           </div>
         </div>
       </div>
