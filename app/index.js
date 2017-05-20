02e97b324d004b8e93ae74f6aa6f93031148786f
norubidium <mshen@cse.unsw.edu.au>
2017-05-20 16:11:23 +1000

    Add login

--- app/index.js
+++ app/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Auth0Lock from 'auth0-lock';
 import { Provider } from 'react-redux';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import Main from 'containers/Main';
 import store from "store";
+import { loadProfile } from 'actions/user/login';
 
+
+const lock = new Auth0Lock('mIfSPvMFfjb23JvgOeZU45qAkcNPEkXS', 'seng3011.au.auth0.com', {});
+
+const cleanUp = () => {
+  localStorage.removeItem("userInfo");
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("id_token");
+  return (<div>Hi, this is me</div>);
+};
+
+const access = (route) => {
+  console.log('THIS IS GREAT!');
+  const { params } = route.match;
+  console.log(route);
+  localStorage.setItem('access_token', params['accessToken']);
+  localStorage.setItem('id_token', params['idToken']);
+  store.dispatch({type:"LOGGEDIN",payload:params.idToken});
+  console.log(lock);
+  store.dispatch(loadProfile(lock, params.idToken));
+  return (<Redirect to="/"/>);
+}
+
+const loadProf = () => {
+  // load profile
+  // const lock = this.props.lock;
+
+  if(localStorage.getItem("id_token")){
+    store.dispatch(loadProfile(lock, localStorage.getItem("id_token")));
+  }
+}
+
+
+//
 class App extends React.Component {
   render () {
     return (
       <Provider store={store}>
         <Router>
-          <Route component={Main} />
+          <Switch>
+            <Route path="/access_token=:accessToken&expires_in=:expires&id_token=:idToken&token_type=:tokenType&state=:state" render={access}/>
+            <Route path="/clean" render={cleanUp} />
+            <Route path='/' component={Main} />
+          </Switch>
         </Router>
       </Provider>
     );
--- app/reducers/user/index.js
+++ app/reducers/user/index.js
@@ -1,14 +1,28 @@
+import Auth0Lock from 'auth0-lock';
 import { actionTypes } from 'actions/user/follow';
 
-const default_state = {
-  loading: false,
-  loaded: true,
-  error: false,
-  /* Stub data for testing display*/
-  user: 'blah',
-  following: [],
-  /* ... add more! */
-};
+const lock = new Auth0Lock('mIfSPvMFfjb23JvgOeZU45qAkcNPEkXS', 'seng3011.au.auth0.com',{
+    theme: {
+      // logo: 'logo/Sauce.png',
+      primaryColor: '#446CB3',
+      title: "Log in",
+      name: "StockOverflow",
+    }
+  });
+
+const user = localStorage.getItem("userInfo") ?
+  {
+    ...JSON.parse(localStorage.getItem("userInfo")),
+    lock,
+  }:
+  {
+    lock,
+    following: [],
+    profile:{},
+  };
+
+
+const default_state = user;
 
 export default (state=default_state, action) => {
   const { type, payload } = action;
@@ -23,6 +37,27 @@ export default (state=default_state, action) => {
         ...state,
         following: state.following.filter((e) => e != payload),
       };
+      case "LOGIN": {
+        return {
+          ...state,
+          token: action.payload,
+        }
+      }
+      case "LOGGEDIN": {
+        const user = {... state, token: action.payload};
+        localStorage.setItem("userInfo", JSON.stringify(user));
+        return user;
+      }
+      case "LOGOUT": {
+        const user = {...state, token: null};
+        localStorage.setItem("userInfo", JSON.stringify(user));
+        return user;
+      }
+      case "LOAD_PROFILE_FINISH": {
+        const user = {...state, profile:action.payload};
+        localStorage.setItem("userInfo", JSON.stringify(user));
+        return user;
+      }
   }
   return state;
 };
