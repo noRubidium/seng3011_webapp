be875de5d94e42a0686feccc827d7e1d6bd5dbd5
norubidium <mshen@cse.unsw.edu.au>
2017-05-21 11:39:31 +1000

    delete stats

--- app/components/FollowButton/index.js
+++ app/components/FollowButton/index.js
@@ -24,7 +24,7 @@ export default class FollowButton extends React.Component {
     const handle = followed ? this.unfollow.bind(this) : this.follow.bind(this);
     const text = followed ? 'Unfollow': 'Follow';
     const follow_button = (
-      <button onClick={handle} href='#' className='btn btn-default follow-button'> { text } the company</button>
+      <button onClick={handle} href='#' className='btn btn-default follow-button'>{ text }</button>
     )
     return follow_button;
   }
