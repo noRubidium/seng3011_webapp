98ec44716be47a8dc5750022a2332f1ffbb1ee18
norubidium <mshen@cse.unsw.edu.au>
2017-05-21 20:46:23 +1000

    haha

--- app/components/Compare/index.js
+++ app/components/Compare/index.js
@@ -2,18 +2,27 @@ import React from 'react';
 
 export default class CompareStats extends React.Component {
   render () {
+    const { stddevs, returns, love } = this.props;
+
     return (
       <div className='panel panel-default'>
         <div className='panel-body compare-stats-panel'>
           <div className='row'>
             <div className='col-sm-4 compare-stats'>
-              <div className='compare-stats-sub-title sub-title'>Stats #1</div>
+              <div className='compare-stats-sub-title sub-title'>
+                Risk Level
+              </div>
+              {}
             </div>
             <div className='col-sm-4 compare-stats side-border'>
-              <div className='compare-stats-sub-title sub-title'>Stats #2</div>
+              <div className='compare-stats-sub-title sub-title'>
+                Return level
+              </div>
             </div>
             <div className='col-sm-4 compare-stats'>
-              <div className='compare-stats-sub-title sub-title'>Stats #3</div>
+              <div className='compare-stats-sub-title sub-title'>
+                Stats #3
+              </div>
             </div>
           </div>
         </div>
