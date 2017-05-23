68805c26f4f9420e11aac8828051ca6bbd229e3b
norubidium <mshen@cse.unsw.edu.au>
2017-05-23 12:53:44 +1000

    company standard deviation

--- app/components/CompanyStats/index.js
+++ app/components/CompanyStats/index.js
@@ -3,8 +3,11 @@ import { connect } from 'react-redux';
 
 import LoadableComponent from 'components/LoadableComponent';
 import { load_company_stats } from 'actions/company/stats';
+import { getStandardDev, getMean } from 'utils/statsUtil';
 
 import StockChartFlag from 'components/StockChart/flag.js';
+
+
 
 @connect((store) => {
   return {
@@ -16,7 +19,10 @@ export default class CompanyInfo extends LoadableComponent {
   constructor (props) {
     super(props);
     this.loaded_object = null;
-    this.state = {};
+    this.state = {
+      min: 0,
+      max: new Date(),
+    };
   }
 
   componentWillMount() {
@@ -41,7 +47,7 @@ export default class CompanyInfo extends LoadableComponent {
   render () {
     const { loaded, company_name } = this.props;
     if (loaded) {
-      this.other_child = <CompanyStatistics {...this.state}/>;
+      this.other_child = <CompanyStatistics {...this.state} data={this.props.financeData}/>;
       this.chart = <StockChartFlag financeData={this.props.financeData}
       company_name={company_name} newsData={this.props.news.news} updateRange={this.updateRange.bind(this)}/>;
       this.loaded_object = (<div>
@@ -54,9 +60,26 @@ export default class CompanyInfo extends LoadableComponent {
 }
 
 
+const toCompanyStatsItems = (data, min, max, f) => {
+  return data
+  .map((d) => {
+    return {label: d.label, value: f(d.values, min, max)};
+  })
+  .map((d) =>
+    <StatCompareItem company={d.label} value={d.value.toFixed(2)}/>
+  );
+}
+
 class CompanyStatistics extends React.Component {
   render () {
-    const { min, max } = this.props;
-    return (<div>min: {min}, max: {max}</div>);
+    const { min, max, data } = this.props;
+    const minDate = new Date(min);
+    const maxDate = new Date(max);
+    console.log('DATES: ', min, max, minDate, maxDate);
+
+    return (<div>
+      Current date range: {minDate.toISOString().split('T')[0]} - {maxDate.toISOString().split('T')[0]}
+      <p>standard Dev: {getStandardDev(data, minDate, maxDate)}</p>
+    </div>);
   }
 }
