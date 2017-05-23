f921b1b757f060c4710349b0372e8378894673bc
Jessica Theodosius <z5057844@student.unsw.edu.au>
2017-05-23 23:08:57 +1000

    learning centre

--- app/containers/Main/nav.js
+++ app/containers/Main/nav.js
@@ -28,14 +28,14 @@ class SideBar extends React.Component {
 
   render() {
     const { path, token } = this.props;
-    const active = path === '/feeds' ? 'feeds' : (path === '/preferences' ? 'preferences' : path === '/training' ? 'training' : 'discover');
-    const base_links = [['discover', true], ['feeds', false], ['preferences', false], ['training', false]];
+    const active = path === '/feeds' ? 'feeds' : (path === '/preferences' ? 'preferences' : path === '/learning-centre' ? 'learning-centre' : 'discover');
+    const base_links = [['discover', true], ['feeds', false], ['preferences', false], ['learning-centre', false]];
     const links = base_links.filter((e) => e[1] || token).map((e) => e[0]);
     const sideLinks = links.map((link, i) =>
       <li className={link + '-sidebar sidebar ' + (active === link ? 'active' : '')} key={i}>
         <Link to={`/${link}`}>
           <img src={`static/images/${link}.svg`}
-          className={`icon`}/>{link}
+          className={`icon`}/>{link.replace('-',' ')}
         </Link>
       </li>
     )
