92fa77c41c0e180967790c29ca7bf43773cc94f2
norubidium <mshen@cse.unsw.edu.au>
2017-05-24 12:30:42 +1000

    shitty learning

--- app/containers/Playground/index.js
+++ app/containers/Playground/index.js
@@ -1,15 +1,63 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+import StockChartFlag from 'components/StockChart/flag.js';
+import IndustryChart from 'components/Industry/chart';
+import { getCmp, getType } from 'utils/lookup';
+import { load_companies, load_abs_stats } from 'actions/company_list';
+import { load_company_stats } from 'actions/company/stats';
+
+const question_style = {
+  'text-align': 'center',
+}
+
+const ans_button_style = {
+  'margin': '1em',
+  'min-width': '2em',
+}
+
+@connect((store) => {
+  return {
+    ...store.company.company_stats,
+    news: store.company.company_news,
+  };
+})
 export default class Playground extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      answer: false,
+    }
     const { dispatch } = this.props;
+    const industry = 'DepartmentStores';
     load_abs_stats(dispatch, industry, getType(industry));
+    load_company_stats('MYR.AX', dispatch, this.props);
+
+  }
+  setAns () {
+    this.setState({answer: true});
   }
   render () {
-    return (<div>
-      <div>Household Goods</div>
-
+    const qa = this.state.answer ? (<div style={question_style}>
+      Sorry, the stock price will go down, as the growth of the household goods section is nearly zero, the industry is currently experiencing a recession.
+    </div>) : (<div style={question_style}>
+      <div> Do you think Myer's stock price will go up or down in the next 5 months? </div>
+      <button style={ans_button_style} className='btn btn-success' onClick={this.setAns.bind(this)}>Yes</button>
+      <button style={ans_button_style} className='btn btn-success' onClick={this.setAns.bind(this)}>No</button>
+    </div>);
+    const { industry:category } = this.props;
+    return (<div style={{'text-align': 'center', 'font-size': 'medium'}}>
+      <div style={{'font-size': 'larger', 'margin': '1em'}}><strong>Household Goods</strong></div>
+      <div className='row'>
+        <div className='col-sm-6'>
+          <IndustryChart industry={category}/>
+        </div>
+        <div className='col-sm-6'>
+        <StockChartFlag financeData={this.props.financeData || []}
+        company_name={'MYER'} newsData={[]}/>
+        </div>
+      </div>
+        {qa}
       </div>);
   }
 }
