322e562ccf08ddc69dc37bbf19f4370688963dc9
norubidium <mshen@cse.unsw.edu.au>
2017-05-29 12:00:10 +1000

    Fix search bar style

--- app/components/Port/Game.js
+++ app/components/Port/Game.js
@@ -1,6 +1,10 @@
 import React, {PropTypes} from 'react';
 
 import SearchCompanyPanel from './SearchCompanyPanel';
+import Holdings from './Holdings';
+import csv2json from 'utils/csv2json';
+
+const format_date = (d) => d.toISOString().split('T')[0];
 
 export default class PortGame extends React.Component {
   constructor(props) {
@@ -14,20 +18,38 @@ export default class PortGame extends React.Component {
   }
 
   updateCompany(company) {
-    this.setState({company});
+    if (company === this.state.company) return;
+    this.setState({company, loading: true, loaded: false, error: false});
+    const start_date = new Date(this.props.date);
+    start_date.setYear(start_date.getFullYear() - 1);
+    const end_date = this.props.date;
+    fetch(`${process.env.API_URL}/cmp/${company}.AX/${format_date(start_date)}/${format_date(end_date)}`)
+    .then((r) => r.text())
+    .then((d) => {
+      if (this.state.company !== company) return;
+      if (!d) {
+        this.setState({loading: false, error: true});
+      }
+      const result = csv2json(d);
+      this.setState({loading: false, loaded: true, data: result});
+      console.log(result);
+    })
+    .catch((e) => {
+      if (this.state.company !== company) return;
+      this.setState({loading: false, error: true});
+    })
   }
 
   render() {
     return (<div>
       <div> This is the game </div>
-      <div> current date is : {this.props.date.toString()} </div>
+      <div> current date is : {this.props.date.toISOString().split('T')[0]} </div>
       <div className="row">
         <div className="col-sm-12 col-md-6">
-          <SearchCompanyPanel {...this.props} updateCompany={this.updateCompany.bind(this)}/>
+          <SearchCompanyPanel {...this.state} updateCompany={this.updateCompany.bind(this)}/>
         </div>
         <div className="col-sm-12 col-md-6">
-          Panel for result and current management.
-          <p> Current company is: {this.state.company}</p>
+          <Holdings { ...this.state } />
         </div>
       </div>
     </div>);
--- app/components/Port/SearchCompanyPanel.js
+++ app/components/Port/SearchCompanyPanel.js
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 
 import SearchBar from 'components/SearchBar';
+import StockChartFlag from 'components/StockChart/flag';
 
 export default class SearchCompanyPanel extends React.Component {
   constructor(props) {
@@ -8,9 +9,14 @@ export default class SearchCompanyPanel extends React.Component {
   }
 
   render() {
+    const { data, company, addStock=(e)=>e } = this.props;
+    const chart = data ? (<StockChartFlag  financeData={data}
+    company_name={company} newsData={[]}/>) : <div>Please search for a company you are interested in investing</div>;
     return (
       <div>
         <SearchBar callback={this.props.updateCompany}/>
+        <button onClick={addStock} > add to my Portfolio </button> 
+        {chart}
       </div>
     );
   }
