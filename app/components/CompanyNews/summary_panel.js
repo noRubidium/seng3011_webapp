7c8ff30b02f0b5d226e8aabf4062dfe35e013b7d
Clinton Hadinata <csghadinata@gmail.com>
2017-05-30 16:56:04 +1000

    Added sentiment summary

--- app/components/CompanyNews/summary_panel.js
+++ app/components/CompanyNews/summary_panel.js
@@ -10,7 +10,7 @@ export default class SummaryPanel extends LoadableComponent {
   }
 
   render() {
-    const { emotion={} } = this.props.data;
+    const { emotion={}, sentiment={} } = this.props.data;
     const info = [];
 
     for (let key in emotion) {
@@ -25,21 +25,28 @@ export default class SummaryPanel extends LoadableComponent {
             plotBackgroundColor: null,
             plotBorderWidth: null,
             plotShadow: false,
-            type: 'pie'
+            type: 'pie',
+            height: '220px'
         },
         title: {
-            text: 'Summary of recent news\'s emotion'
+            text: 'Emotion Breakdown',
+            style: {'fontSize':'16px'}
+        },
+        subtitle: {
+          text: '(of recent news articles)'
         },
         tooltip: {
             pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
         },
         plotOptions: {
             pie: {
+                size: '115px',
                 allowPointSelect: true,
                 cursor: 'pointer',
                 dataLabels: {
                     enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    distance: 7,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f}%',
                     style: {
                         color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
                     }
@@ -53,8 +60,62 @@ export default class SummaryPanel extends LoadableComponent {
          }]
     }
 
+    const sentimentInfo = [];
+    for (let key in sentiment) {
+        sentimentInfo.push({
+            name: key,
+            y: sentiment[key]['count']
+        });
+    }
+
+    console.log(this.props);
+
+    const sentimentConfig = {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false,
+            height: '180px'
+        },
+        colors: ['green', '#da0b0b'],
+        title: {
+            text: 'Sentiment Breakdown',
+            style: {'fontSize':'16px'}
+        },
+        subtitle: {
+          text: '(of recent news articles)'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                size: '160px',
+                dataLabels: {
+                    enabled: true,
+                    distance: 5
+                },
+                startAngle: -90,
+                endAngle: 90,
+                center: ['50%', '100%']
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Sentiment Share',
+            innerSize: '65%',
+            data: sentimentInfo
+        }]
+
+    }
+
     this.loaded_object = (<div>
-        <ReactHighcharts config={config} />
+        <div style={{'height':'50%'}}>
+          <ReactHighcharts config={sentimentConfig} />
+        </div>
+        <div style={{'height':'50%'}}>
+          <ReactHighcharts config={config} />
+        </div>
     </div>);
     return super.render();
   }
